41ccffd24b07e1ee63e0b064f717fd95bac09b06
psq <851205583@qq.com>
2019-07-12 09:23:16 +0800

    eclipse提交项目

---
+++ src/asd/aaaaaaa.java
@@ -0,0 +1,20 @@
+package asd;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class aaaaaaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a=45.56f;
+		@SuppressWarnings("all")
+		long aa=1212;
+		int b=152;
+		Map map=new HashMap();
+		map.put("ds", "d");
+		System.out.println(b+a+"ºÍ£º");
+		System.out.println(map.get("ds"));
+	}
+
+}
